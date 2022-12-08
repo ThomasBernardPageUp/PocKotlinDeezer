c2a1e4561d34fa5d050def855e722de9e1268587
Thomas BERNARD <tbernard@pageup.fr>
2022-12-08 15:56:09 +0100

    Get artists in fragment

--- app/src/main/java/com/example/deezer/screens/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/deezer/screens/fragments/home/HomeFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import com.example.deezer.R
 
 class HomeFragment : Fragment() {
+
+    var searchView : SearchView? = null
 
     companion object {
         fun newInstance() = HomeFragment()
@@ -20,16 +23,34 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
 
-        viewModel.searchArtist("Damso", {
-            var x = it
+
+        searchView = this.view?.findViewById(R.id.homeSearchView)
+
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                viewModel.searchArtist(p0 ?: "", {
+                    var x = it
+                })
+
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+
         })
 
     }
